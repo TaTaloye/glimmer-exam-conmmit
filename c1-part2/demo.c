9c664db780752e1a72bd9af804c9ba0391f7c765
TaTaloye <z2955766786@qq.com>
2026-09-20 22:26:06 +0800

    c1-p2

--- c1-part2/demo.c
+++ c1-part2/demo.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 int main(){
-    float a = 3;
-    printf("%.2f",a);
+    char name[20];
+    int age;
+    char return_value;
+    while (true){
+    printf("年龄：");
+    scanf("%d",&age);
+    printf("姓名：");
+    scanf("%s",name);
+    printf("是否进行修改(y/n)：");
+    scanf(" %c",&return_value);
+    if (return_value == 'y' || return_value == 'Y'){
+        printf("年龄：");
+        scanf("%d",&age);
+        printf("姓名：");
+        scanf("%s",name);
+    }
+    else if (return_value == 'n' || return_value == 'N'){
+        printf("你的年龄：%d\n",age);
+        printf("你的姓名：%s\n",name);
+        return 0;
+    }
+    else{
+        printf("小调皮，只能输入y/n哦\n");
+    }
+}
 }
